2002227a179274df7afde9418d0fdff9dc305db1
Walid Jamarin <walidjamarin21@gmail.com>
2021-01-03 17:00:40 +0800

    add functionality for google maps api and setup the project

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0LxzeoFIwnIL0P_Q1QafyxgF2KYA7gYM",
+  authDomain: "geo-ninjas-47801.firebaseapp.com",
+  projectId: "geo-ninjas-47801",
+  storageBucket: "geo-ninjas-47801.appspot.com",
+  messagingSenderId: "216349336144",
+  appId: "1:216349336144:web:1f633bb178e56482816755"
+};
+
+const firebaseApp = firebase.initializeApp(config)
+
+export default firebaseApp.firestore()
